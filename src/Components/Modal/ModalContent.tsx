ea536a40b9abef3d0eb403d01a3709a86358149c
diogomarv <diogomarv@gmail.com>
2022-10-09 14:57:35 -0300

    fix: removed unnecessary files and started styling modal

--- src/Components/Modal/ModalContent.tsx
+++ src/Components/Modal/ModalContent.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import styles from 'Modal.module.css';
+import styles from './ModalContent.module.css';
 
 const ModalContent = ({ closeModal }: any) => {
   return (
-    <div>
-      <button onClick={closeModal}>Close</button>
-      <h1>Title Modal Example</h1>
+    <div className={styles.modalBox}>
+      <div className={styles.modalTop}>
+        <h2>Title Modal Example</h2>
+        <button onClick={closeModal}>Close</button>
+      </div>
+
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quo enim, explicabo deserunt saepe impedit molestias animi cum unde omnis quos cumque aut, minima consequatur corrupti quae. Possimus, temporibus illo!</p>
 
     </div>
